2524aeb0fc81592f5cf40b1ed9db93f07363344b
Narathorn Noophum <6310210181@psu.ac.th>
2023-08-11 19:26:42 +0700

    init commit

---
+++ src/test/java/com/boardgame/main/BoardgameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardgame.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardgameBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
